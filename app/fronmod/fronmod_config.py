654633f6e0ccbfde81c6856f685052dd548940d8
Raul Rosenlöcher <git@home>
2019-01-06 17:55:07 +0100

    restructure fronmod config

--- app/fronmod/fronmod_config.py
+++ app/fronmod/fronmod_config.py
@@ -15,6 +15,7 @@ class FronmodConstants:
     # mppt
     ITEM_MPPT_BAT_POWER = 'valPvBatPower'
     ITEM_MPPT_BAT_STATE = 'valPvMpptBatState'
+    ITEM_MPPT_MOD_POWER = 'valPvModPower'
     ITEM_MPPT_MOD_STATE = 'valPvMpptModState'
     ITEM_MPPT_MOD_VOLTAGE = 'valPvModVoltage'
     RAW_MPPT_BAT_POWER = 'rawPvMpptBattPower'
@@ -22,12 +23,23 @@ class FronmodConstants:
     RAW_MPPT_MOD_VOLTAGE = 'rawPvMpptModVoltage'
     RAW_MPPT_POWER_SF = 'rawPvMpptPowerSfBase'
     RAW_MPPT_VOLTAGE_SF = 'rawPvMpptVoltageSfBase'
+    SHOW_MPPT_BAT_POWER = 'showPvBatPower'
+    SHOW_MPPT_MOD_POWER = 'showPvModPower'
 
     # meter
     ITEM_MET_AC_FREQUENCY = 'valPvMetAcFrequency'
     ITEM_MET_AC_POWER = 'valPvMetAcPower'  # W
     ITEM_MET_ENERGY_EXP_TOT = 'valPvMetEnergyExpTot'
     ITEM_MET_ENERGY_IMP_TOT = 'valPvMetEnergyImpTot'
+
     SHOW_MET_AC_POWER = 'showPvMetAcPower'  # kW
 
+    # eflow
+    EFLOW_BAT_IN = 'valPvEflowBatIn'
+    EFLOW_BAT_OUT = 'valPvEflowBatOut'
+    EFLOW_INV_AC_IN = 'valPvEflowInvAcIn'
+    EFLOW_INV_AC_OUT = 'valPvEflowInvAcOut'
+    EFLOW_INV_DC_IN = 'valPvEflowInvDcIn'
+    EFLOW_INV_DC_OUT = 'valPvEflowInvDcOut'
+    EFLOW_MOD_OUT = 'valPvEflowModOut'
 
